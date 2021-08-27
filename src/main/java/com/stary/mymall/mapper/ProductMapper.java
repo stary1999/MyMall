8851e4e6ee769501ee38a434f942164ea09799c0
stary1999 <stary2020@163.com>
2021-08-27 23:04:15 +0800

    ajax，前后端分离测试，mybatis plus逆向工程测试

---
+++ src/main/java/com/stary/mymall/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package com.stary.mymall.mapper;
+
+import com.stary.mymall.entity.Product;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author stary
+ * @since 2021-08-27
+ */
+public interface ProductMapper extends BaseMapper<Product> {
+
+}
---
+++ src/main/java/com/stary/mymall/service/IProductService.java
@@ -0,0 +1,17 @@
+package com.stary.mymall.service;
+
+import com.stary.mymall.entity.Product;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author stary
+ * @since 2021-08-27
+ */
+public interface IProductService extends IService<Product> {
+
+
+}
