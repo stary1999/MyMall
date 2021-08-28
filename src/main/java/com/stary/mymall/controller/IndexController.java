454aeb1d44e4feef9ee0f1bb1ed609cef2aedf90
stary1999 <stary2020@163.com>
2021-08-28 16:00:00 +0800

    分页功能实现

--- src/main/java/com/stary/mymall/controller/IndexController.java
+++ src/main/java/com/stary/mymall/controller/IndexController.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.stary.mymall.entity.Product;
 import com.stary.mymall.service.IProductService;
+import com.stary.mymall.utils.MyPageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Controller;
@@ -31,44 +32,18 @@ public class IndexController {
     public ModelAndView index(ModelAndView modelAndView,
                               @RequestParam(value = "pn", defaultValue = "1") Integer pn) {
 
-//        IPage<User> userPage = new Page<>(2, 2);//参数一是当前页，参数二是每页个数
-//        6 userPage = userMapper.selectPage(userPage, null);
-//        7 List<User> list = userPage.getRecords();
-//        8 for (User user : list) {
-//            9 System.out.println(user);
-//            10
-//        }
 
-        Page<Product> page=new Page<>(pn,2);
+        Page<Product> page=new Page<>(pn,1);
         IPage<Product> pageInfo = productService.selectProductPage(page);
+        MyPageHelper myPageHelper=new MyPageHelper(pageInfo);
+        System.out.println("myPagehelper==="+myPageHelper);
 
 
-
-        modelAndView.addObject("pageInfo", pageInfo);
+        modelAndView.addObject("pageInfo", myPageHelper);
         modelAndView.setViewName("index");
         return modelAndView;
     }
 
-//    @RequestMapping("/getIndex")
-//    public ModelAndView Index(ModelAndView modelAndView,
-//                              @RequestParam(value = "pn", defaultValue = "1") Integer pn){
-//        PageHelper.startPage(pn,5);
-//
-//        List<Product> list = productService.list();
-//        PageInfo pageInfo=new PageInfo(list,5);
-//        System.out.println("pageInfo==="+pageInfo);
-//        System.out.println("pageInfoToString==="+pageInfo.toString());
-//
-//
-////        pageInfo===PageInfo{pageNum=1, pageSize=4, size=4, startRow=0, endRow=3, total=4, pages=1,
-////            list=[Product(productId=1, productName=华为p50, productSort=手机, productPrice=5099.00, productStock=100, productImgPath=null), Product(productId=2, productName=小米10, productSort=手机, productPrice=4999.00, productStock=200, productImgPath=null), Product(productId=3, productName=洗衣粉, productSort=百货, productPrice=15.00, productStock=1000, productImgPath=null), Product(productId=4, productName=索尼耳机, productSort=数码, productPrice=399.00, productStock=500, productImgPath=null)],
-////        prePage=0, nextPage=0, isFirstPage=true, isLastPage=true, hasPreviousPage=false, hasNextPage=false, navigatePages=5, navigateFirstPage1, navigateLastPage1, navigatepageNums=[1]}
-////
-//        modelAndView.addObject("pageInfo",pageInfo);
-//
-//        modelAndView.setViewName("index");
-//        return modelAndView;
-//    }
 
 
 }
---
+++ src/main/java/com/stary/mymall/utils/MyPageHelper.java
@@ -0,0 +1,67 @@
+package com.stary.mymall.utils;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.stary.mymall.entity.Product;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * @author stary
+ * @version 1.0
+ * @classname MyPageHelper
+ * @description
+ * @create 2021/8/28-15:26
+ */
+@ToString
+@Data
+public class MyPageHelper {
+
+    //原page参数
+    //当前页码
+    private Long current;
+    //总记录数
+    private Long total;
+    //总页数
+    private Long pages;
+    //返回值
+    private List<?> records;
+    //新增参数
+    //前一页
+    private Long previous;
+    //后一页
+    private Long next;
+    //是否存在前一页
+    private Boolean isPrevious;
+    //是否存在后一页
+    private Boolean isNext;
+
+    public  MyPageHelper(IPage<?> pageInfo){
+        this.current=pageInfo.getCurrent();
+        this.pages=pageInfo.getPages();
+        this.total=pageInfo.getTotal();
+        this.records=pageInfo.getRecords();
+        if (this.current>1){
+            this.previous=this.current-1;
+            this.isPrevious=true;
+        }
+        else {
+            this.previous=1L;
+            this.isPrevious=false;
+        }
+        if (this.current<this.pages){
+            this.next=this.current+1;
+            this.isNext=true;
+        }
+        else {
+            this.next=pages;
+            this.isNext=false;
+        }
+
+    }
+
+
+
+
+}
